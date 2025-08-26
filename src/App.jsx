c5ca56c8487448bb4a57cf4f384a7bfaab3e5d57
Silous <silous666@gmail.com>
2025-08-26 16:14:37 +0200

    add header and footer files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
+    <Header />
       <p>Hello world</p>
+    <Footer />
     </>
   )
 }
