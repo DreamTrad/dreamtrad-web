c0ee13084d5a9b1b5daa3056257998ff131b54ee
Silous <silous666@gmail.com>
2025-08-27 11:42:09 +0200

    fix navlink GameNavBar

--- src/components/game/GameNavBar.jsx
+++ src/components/game/GameNavBar.jsx
@@ -1,40 +1,21 @@
 import { NavLink } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function GameNavbar() {
+  const { id } = useParams();
+
   return (
     <nav className="bg-accent text-light">
       <ul className="flex justify-center max-w-6xl mx-auto">
-        <li>
-          <NavLink
-            to="."
-            end
-            className={({ isActive }) =>
+        <li><NavLink to={`/jeu/${id}`} end className={({ isActive }) =>
               `px-4 py-2 rounded-md ${isActive ? "bg-hover" : ""}`
-            }
-          >
-            Le Jeu
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="patch-fr"
-            className={({ isActive }) =>
+            }>Le Jeu</NavLink></li>
+        <li><NavLink to={`/jeu/${id}/game-fr`} className={({ isActive }) =>
               `px-4 py-2 rounded-md ${isActive ? "bg-hover" : ""}`
-            }
-          >
-            Patch FR
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="guide"
-            className={({ isActive }) =>
+            }>Patch FR</NavLink></li>
+        <li><NavLink to={`/jeu/${id}/guide`} className={({ isActive }) =>
               `px-4 py-2 rounded-md ${isActive ? "bg-hover" : ""}`
-            }
-          >
-            Guide
-          </NavLink>
-        </li>
+            }>Guide</NavLink></li>
       </ul>
     </nav>
   );
