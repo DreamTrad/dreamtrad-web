d94f5e241a549d4e24105518671600625ad71491
Silous <silous666@gmail.com>
2025-09-05 16:40:20 +0200

    reorganisation data paranormasight

--- src/data/jeu/paranormasight/index.js
+++ src/data/jeu/paranormasight/index.js
@@ -1,5 +1,5 @@
-import { achievements } from "./achievements";
-import { team } from "./team";
+import { achievements } from "./guide/achievements";
+import { team } from "./jeufr/team";
 
 
 export const game = {
@@ -9,8 +9,8 @@ export const game = {
     general: {
       name: "Le Jeu",
       sections: [
-        { id: "Presentation", name: "Présentation", file: "presentation" },
-        { id: "auteurs", name: "Auteurs", file: "auteurs" },
+        { id: "Presentation", name: "Présentation", file: "general/presentation" },
+        { id: "auteurs", name: "Auteurs", file: "general/auteurs" },
       ],
     },
 
@@ -29,7 +29,7 @@ export const game = {
         {
           id: "telechargement",
           name: "Télechargement",
-          file: "patch/informations",
+          file: "jeufr/informations",
           platforms: [
             { id: "PC", name: "PC (Windows, Steamdeck, Linux)", link: "https://drive.google.com/uc?export=download&id=1cdCUPpaq5xTrDPoqrCGARqPLOSqu_ud7" },
           ],
@@ -38,9 +38,9 @@ export const game = {
           id: "installation",
           name: "Installation",
           platforms: [
-            { id: "windows", name: "Windows", file: "patch/installation/windows" },
-            { id: "steamdeck", name: "Steam Deck/Linux", file: "patch/installation/steamdeck" },
-            { id: "manuellement", name: "Manuellement", file: "patch/installation/manuellement" },
+            { id: "windows", name: "Windows", file: "jeufr/installation/windows" },
+            { id: "steamdeck", name: "Steam Deck/Linux", file: "jeufr/installation/steamdeck" },
+            { id: "manuellement", name: "Manuellement", file: "jeufr/installation/manuellement" },
           ],
         },
         { id: "equipe", name: "Équipe", data: team },
