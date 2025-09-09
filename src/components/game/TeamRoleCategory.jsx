98a8d4fd88766fbfeb6394a2c684a4518785a196
Silous <silous666@gmail.com>
2025-09-09 14:55:19 +0200

    icon pour réseau équipe

--- src/components/game/TeamRoleCategory.jsx
+++ src/components/game/TeamRoleCategory.jsx
@@ -1,3 +1,6 @@
+import LinkWithIcon from "../ui/LinkWithIcon";
+
+
 export default function TeamRoleCategory({ category }) {
   return (
     <div className="bg-bg-secondary p-4 rounded-md shadow-md space-y-2">
@@ -8,26 +11,8 @@ export default function TeamRoleCategory({ category }) {
             <span className="font-medium">{person.name}</span>
 
             {/* External link icon if link exists */}
-            {person.link && (
-              <a
-                href={person.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-accent hover:text-hover"
-                title="Voir le lien"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 inline-block"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round"
-                  d="M13.828 10.172a4 4 0 010 5.656m-3.657-9.9a4 4 0 015.657 5.657l-1.415 1.415m-2.829 2.829a4 4 0 01-5.657-5.657l1.415-1.415" />
-                </svg>
-              </a>
+           {person.link && (
+              <LinkWithIcon url={person.link} />
             )}
 
             {/* Role only if non-empty */}
