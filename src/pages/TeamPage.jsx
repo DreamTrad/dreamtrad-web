f48046dd5a67efa9eb7f984043d19624030a4207
Silous <silous666@gmail.com>
2025-10-29 16:50:54 +0100

    metatags en plus

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
+import MetaTags from "../components/MetaTags";
 import TeamMemberCard from "../components/card/TeamMemberCard";
 import MarkdownSection from "../components/ui/MarkdownSection";
-import useFetchWithLoader from "../hooks/useFetchWithLoader";
 
 export default function TeamPage() {
   const [team, setTeam] = useState([]);
@@ -15,13 +15,18 @@ export default function TeamPage() {
   }, []);
 
   return (
-    <div>
+    <>
+      <MetaTags
+        title="Équipe"
+        description="Les personnes qui ont beaucoup apportés aux projets et à l’équipe au fil des années."
+        url="equipe"
+      />
       <div className="p-8 max-w-6xl mx-auto">
         <h1 className="text-3xl font-bold text-accent mb-8 text-center">
           Notre Équipe
         </h1>
         <div className="mt-16 mb-16">
-          <MarkdownSection file={file} className="leading-relaxed text-justify"/>
+          <MarkdownSection file={file} className="leading-relaxed text-justify" />
         </div>
       </div>
 
@@ -30,6 +35,6 @@ export default function TeamPage() {
           <TeamMemberCard key={member.id} {...member} />
         ))}
       </div>
-    </div>
+    </>
   );
 }
