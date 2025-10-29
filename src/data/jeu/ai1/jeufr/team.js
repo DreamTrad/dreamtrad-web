9dc4001beee9eb24391095f1666de003a1c13e14
Silous <silous666@gmail.com>
2025-10-29 19:05:22 +0100

    équipe ai1 #18

--- src/data/jeu/ai1/jeufr/team.js
+++ src/data/jeu/ai1/jeufr/team.js
@@ -7,21 +7,45 @@ export const team = [
     {
         "category": "Traducteur",
         "people": [
+            { "name": "jls", "role": "", "link": "" },
+            { "name": "Ritsu", "role": "", "link": "" },
+            { "name": "AlexisTGF", "role": "", "link": "" },
+            { "name": "Daphnée Zou Gaming", "role": "", "link": "" },
+            { "name": "Foubou", "role": "", "link": "" },
+            { "name": "Dratui", "role": "", "link": "" },
+            { "name": "Icaro", "role": "", "link": "" },
         ]
     },
     {
         "category": "Programmeur",
         "people": [
+            { "name": "Lastdreamer", "role": "Outils d’extraction/importation", "link": "" },
         ]
     },
     {
         "category": "Images",
         "people": [
+            { "name": "roli300", "role": "", "link": "" },
+            { "name": "Daphnée Zou Gaming", "role": "", "link": "" },
         ]
     },
     {
         "category": "Relecteur",
         "people": [
+            { "name": "jls", "role": "relecteur", "link": "" },
+            { "name": "Ritsu", "role": "relecteur", "link": "" },
+            { "name": "AlexisTGF", "role": "relecteur", "link": "" },
+            { "name": "Daphnée Zou Gaming", "role": "relecteur", "link": "" },
+            { "name": "Foubou", "role": "relecteur", "link": "" },
+            { "name": "Dratui", "role": "relecteur", "link": "" },
+            { "name": "Icaro", "role": "relecteur", "link": "" },
+        ]
+    },
+    {
+        "category": "Autre",
+        "people": [
+            { "name": "Paul", "role": "animation 3D d’Aiba pour vidéo bande-annonce", "link": "" },
+            { "name": "Céliecover", "role": "interprétation de la version française de Invicible Rainbow Arrow", "link": "" },
         ]
     },
 ];
