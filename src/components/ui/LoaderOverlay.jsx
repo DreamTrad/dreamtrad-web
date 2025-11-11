630023c2d1632ba79ef0b02fdf5bdcf261545d0d
Silous <silous666@gmail.com>
2025-11-11 18:34:53 +0100

    loader supr bg

--- src/components/ui/LoaderOverlay.jsx
+++ src/components/ui/LoaderOverlay.jsx
@@ -1,7 +1,7 @@
 export default function LoaderOverlay({ className = "" }) {
   return (
     <div
-      className={`bg-bg-tertiary flex items-center justify-center rounded-lg p-4 ${className}`}
+      className={`flex items-center justify-center rounded-lg p-4 ${className}`}
     >
       <div className="border-accent h-12 w-12 animate-spin rounded-full border-4 border-t-transparent"></div>
     </div>
