74abc0875cd1b1fda196ec97bb0fab38c29a898a
Silous <silous666@gmail.com>
2026-01-11 16:25:24 +0100

    entrée gare saihate

--- src/components/ui/LinkWithIcon.jsx
+++ src/components/ui/LinkWithIcon.jsx
@@ -4,47 +4,21 @@ import Link from "next/link";
 const localSiteIcon = "/dreamtrad-logo-rond.png";
 
 const iconMap = [
-  {
-    match: ["discord.gg", "discord.com"],
-    icon: "/icons/website/discord.svg",
-    label: "Discord",
-  },
+  { match: ["apps.apple.com"], icon: "/icons/website/apple.svg", label: "App Store" },
+  { match: ["discord.gg", "discord.com"], icon: "/icons/website/discord.svg", label: "Discord" },
   { match: ["bsky.app"], icon: "/icons/website/bluesky.svg", label: "Bluesky" },
   { match: ["github.com"], icon: "/icons/website/github.svg", label: "GitHub" },
-  {
-    match: ["instagram.com"],
-    icon: "/icons/website/instagram.svg",
-    label: "Instagram",
-  },
+  { match: ["instagram.com"], icon: "/icons/website/instagram.svg", label: "Instagram" },
   { match: ["itch.io"], icon: "/icons/website/itchio.svg", label: "Itch.io" },
-  {
-    match: ["nintendo.com"],
-    icon: "/icons/website/nintendo.ico",
-    label: "Nintendo",
-  },
-  {
-    match: ["store.steampowered.com"],
-    icon: "/icons/website/steam.svg",
-    label: "Steam",
-  },
+  { match: ["nintendo.com"], icon: "/icons/website/nintendo.ico", label: "Nintendo" },
+  { match: ["store.steampowered.com"], icon: "/icons/website/steam.svg", label: "Steam" },
   { match: ["twitch.tv"], icon: "/icons/website/twitch.svg", label: "Twitch" },
   { match: ["vndb.org"], icon: "/icons/website/vndb.ico", label: "VNDB" },
   { match: ["xbox.com"], icon: "/icons/website/xbox.svg", label: "Xbox" },
-  {
-    match: ["playstation.com"],
-    icon: "/icons/website/playstation.svg",
-    label: "PlayStation",
-  },
-  {
-    match: ["twitter.com", "x.com"],
-    icon: "/icons/website/xitter.svg",
-    label: "X / Twitter",
-  },
-  {
-    match: ["youtube.com", "youtu.be"],
-    icon: "/icons/website/youtube.svg",
-    label: "YouTube",
-  },
+  { match: ["playstation.com"], icon: "/icons/website/playstation.svg", label: "PlayStation" },
+  { match: ["play.google.com"], icon: "/icons/website/googleplay.svg", label: "Google Play" },
+  { match: ["twitter.com", "x.com"], icon: "/icons/website/xitter.svg", label: "X / Twitter" },
+  { match: ["youtube.com", "youtu.be"], icon: "/icons/website/youtube.svg", label: "YouTube" },
   { match: ["gog.com"], icon: "/icons/website/gog.svg", label: "GOG" },
 ];
 
