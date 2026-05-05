f71b34528a0b3ffce9f79c5f79413184c57f8614
Silous <silous666@gmail.com>
2026-05-05 07:26:06 +0200

    supabase service

--- src/app/api/admin/invite-user/route.js
+++ src/app/api/admin/invite-user/route.js
@@ -1,13 +1,8 @@
 // src/app/api/admin/invite-user/route.js
 
 import { NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
+import { supabaseAdmin } from "@/lib/supabase/service";
 import { createClient as createClientServer } from "@/lib/supabase/server";
-
-const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,
-);
 
 export async function POST(request) {
 
