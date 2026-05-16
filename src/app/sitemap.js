79afe51cb7e1b0d8f30fdf09791e36840b02c25c
Silous <silous666@gmail.com>
2026-05-16 09:22:57 +0200

    sitemap force static

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -9,6 +9,8 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
+export const dynamic = "force-static";
+
 export default async function sitemap() {
   const staticPages = [
     "",
